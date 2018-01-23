9d113eea4b655d1ecd4c924d0bbb4fb2dc4b55a1
Michael Meffie <mmeffie@sinenomine.net>
2018-01-22 21:41:05 -0500

    codebreaker game

---
+++ codebreaker/codebreaker.py
@@ -0,0 +1,35 @@
+#
+# Codebreaker game (aka cows and bulls)
+#
+
+import random
+
+digits = ['0','1','2','3','4','5','6','7','8','9']
+random.shuffle(digits)
+code = digits[0:4]
+
+print("""
+Try to find the secret code in time (4 digits, all different).
+I will print an 'x' for each correct digit in the correct position
+and will print an 'o' for each correct digit but in the wrong position.
+""")
+for trial in range(20, 0, -1):
+    guess = list(input("({0}) > ".format(trial)))
+    if guess == code:
+        break
+    if len(guess) != len(code):
+        print("enter 4 digits")
+        continue
+    hits = ''
+    misses = ''
+    for i,d in enumerate(guess):
+        if d == code[i]:
+            hits += 'x'
+        elif d in code:
+            misses += 'o'
+    print(''.join((hits, misses)))
+
+if guess == code:
+    print("Success")
+else:
+    print("Times up.")
