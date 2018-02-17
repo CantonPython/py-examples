4310afafdbfb7f31b84af201ee56c1672d36b824
Michael Meffie <mmeffie@sinenomine.net>
2018-02-17 10:31:47 -0500

    boggle reorg
    move helper functions, conts, and word list into a module.

---
+++ boggle/main.py
@@ -0,0 +1,61 @@
+import string
+import boggle
+
+def make_index(board):
+    index = {}
+    for letter in string.ascii_lowercase:
+        index[letter] = []
+    for i,letter in enumerate(board):
+        index[letter].append(i)
+    return index
+
+def find_word(index, word):
+    first = word[0]
+    rest = word[1:]
+    for i in index[first]:
+        found = find_subword(index, [i], rest)
+        if found:
+            return found
+    return None
+
+def find_subword(index, path, word):
+    if not word:
+        return path
+    tail = path[-1]
+    first = word[0]
+    rest = word[1:]
+    for i in index[first]:
+        if i in boggle.adjacent[tail] and i not in path:
+            found = find_subword(index, path+[i], rest)
+            if found:
+                return found
+    return None
+
+def solve(board):
+    solution = {}
+    index = make_index(board)
+    for word in boggle.words:
+        path = find_word(index, word)
+        if path:
+            solution[word] = path
+    return solution
+
+def main():
+    board = boggle.new_board()
+    boggle.show_board(board)
+    guess = set()
+    print('? = show board, . = quit')
+    while True:
+        w = input("boggle> ").strip()
+        if w == '.':
+            break
+        elif w == '?':
+            boggle.show_board(board)
+        else:
+            guess.add(w)
+    solution = set(solve(board).keys())
+    print("found: ", guess & solution)
+    print("missed: ", solution - guess)
+
+if __name__ == '__main__':
+    main()
