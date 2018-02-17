e395665a88716b0dde3fbef1be155c76d0697189
Michael Meffie <mmeffie@sinenomine.net>
2018-02-17 09:47:53 -0500

    main function for codebreaker game

--- codebreaker/codebreaker.py
+++ codebreaker/codebreaker.py
@@ -4,32 +4,35 @@
 
 import random
 
-digits = ['0','1','2','3','4','5','6','7','8','9']
-random.shuffle(digits)
-code = digits[0:4]
 
-print("""
+def main():
+    print("""
 Try to find the secret code in time (4 digits, all different).
 I will print an 'x' for each correct digit in the correct position
 and will print an 'o' for each correct digit but in the wrong position.
 """)
-for trial in range(9, 0, -1):
-    guess = list(input("({0}) > ".format(trial)))
+    digits = ['0','1','2','3','4','5','6','7','8','9']
+    random.shuffle(digits)
+    code = digits[0:4]
+    for trial in range(9, 0, -1):
+        guess = list(input("({0}) > ".format(trial)))
+        if guess == code:
+            break
+        if len(guess) != len(code):
+            print("enter 4 digits")
+            continue
+        hits = ''
+        misses = ''
+        for i,d in enumerate(guess):
+            if d == code[i]:
+                hits += 'x'
+            elif d in code:
+                misses += 'o'
+        print(' '*12 + ''.join((hits, misses)))
     if guess == code:
-        break
-    if len(guess) != len(code):
-        print("enter 4 digits")
-        continue
-    hits = ''
-    misses = ''
-    for i,d in enumerate(guess):
-        if d == code[i]:
-            hits += 'x'
-        elif d in code:
-            misses += 'o'
-    print(' '*12 + ''.join((hits, misses)))
+        print("Success")
+    else:
+        print("Times up. code was", code)
 
-if guess == code:
-    print("Success")
-else:
-    print("Times up. code was", code)
+if __name__ == '__main__':
+    main()
